a53648a2724b0491d50a02224c7470aeec53d647
NamAgarwal <86967304+NamAgarwal@users.noreply.github.com>
2022-09-18 12:04:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,23 +16,10 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
 
-    poseNet = ml5.poseNet(video, modeLoaded);
-    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video, 0, 0, 600, 500);
 }
 function modeLoaded(){
     console.log("Posenet is initialized");
-}
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results);
-        left_wrist_x = results[0].pose.leftWrist.x;
-        left_wrist_y = results[0].pose.leftWrist.y;
-        console.log("Left wrist x = "+ left_wrist_x + " Left wrist y = "+ left_wrist_y);
-        right_wrist_x = results[0].pose.rightWrist.x;
-        right_wrist_y = results[0].pose.rightWrist.y;
-        console.log("Right wrist x = "+ right_wrist_x + " Right wrist y = "+ right_wrist_y);
-    }
 }
